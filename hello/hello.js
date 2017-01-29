a909b83db5c8cc5f525db311883258a400371d25
peke2 <abem@dc4.so-net.ne.jp>
2017-01-30 01:01:41 +0900

    glMatrixのソースを参照していたのでパッケージを参照するよう修正。 シェーダーとビュー、投影マトリクスを追加。 目標の正三角形の描画までできた。

--- hello/hello.js
+++ hello/hello.js
@@ -1,5 +1,9 @@
 var gl;
 
+var vertexPositionAttribute;
+var vertexColorAttribute;
+var shaderProgram;
+
 function start()
 {
 	var canvas = document.getElementById("glcanvas");
@@ -11,18 +15,55 @@ function start()
 	gl.enable(gl.DEPTH_TEST);
 	gl.depthFunc(gl.LEQUAL);
 
+
+	var vertexShaderSource = 
+	'attribute vec3 position;\n'+
+	'attribute vec4 color;\n'+
+	'uniform mat4 viewMatrix;\n'+
+	'uniform mat4 projMatrix;\n'+
+	'varying lowp vec4 vColor;\n'+
+	'void main(){\n'+
+	'gl_Position = projMatrix * viewMatrix * vec4(position, 1.0);\n'+
+	'vColor = color;\n'+
+	'}';
+
+	var fragmentShaderSource = 
+	'varying lowp vec4 vColor;\n'+
+	'void main(){\n'+
+	'gl_FragColor = vColor;\n'+
+	'}';
+
+	var vertexShader = createShader(gl, vertexShaderSource, gl.VERTEX_SHADER);
+	var fragmentShader = createShader(gl, fragmentShaderSource, gl.FRAGMENT_SHADER);
+
+	shaderProgram = gl.createProgram();
+	gl.attachShader(shaderProgram, vertexShader);
+	gl.attachShader(shaderProgram, fragmentShader);
+	gl.linkProgram(shaderProgram);
+
+	if( !gl.getProgramParameter(shaderProgram, gl.LINK_STATUS) )
+	{
+		console.log('シェーダープログラムのリンクに失敗しました。'+gl.getProgramInfoLog(shaderProgram));
+	}
+
+	gl.useProgram(shaderProgram);
+	vertexPositionAttribute = gl.getAttribLocation(shaderProgram, 'position');
+	gl.enableVertexAttribArray(vertexPositionAttribute);
+	vertexColorAttribute = gl.getAttribLocation(shaderProgram, 'color');
+	gl.enableVertexAttribArray(vertexColorAttribute);
+
+	initBuffer();
+
 	setInterval(drawScene, 16);
 }
 
 function initWebGL(canvas)
 {
 	gl = null;
-	try
-	{
+	try{
 		gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
 	}
-	catch(e)
-	{
+	catch(e){
 	}
 
 	if( !gl )
@@ -33,14 +74,102 @@ function initWebGL(canvas)
 	return	gl;
 }
 
+function createShader(gl, source, type)
+{
+	var shader = gl.createShader(type);
+	gl.shaderSource(shader, source);
+	gl.compileShader(shader);
+	if( !gl.getShaderParameter(shader, gl.COMPILE_STATUS) )
+	{
+		var info = gl.getShaderInfoLog(shader);
+		throw 'Shaderをコンパイルできませんでした。\n\n'+info;
+	}
+	return	shader;
+}
+
+
+var vertexBuffer;
+var colorBuffer;
+
+function initBuffer()
+{
+	var vertices = [
+		 0.0,  0.578, 0,
+		 0.5, -0.288, 0,
+		-0.5, -0.288, 0,
+	];
+	var colors = [
+		 1.0, 0.0, 0.0, 1.0,
+		 0.0, 1.0, 0.0, 1.0,
+		 0.0, 0.0, 1.0, 1.0,
+	];
+	//var vertexBuffer = gl.createBuffer();
+	//var colorBuffer = gl.createBuffer();
+
+	vertexBuffer = gl.createBuffer();
+	colorBuffer = gl.createBuffer();
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+}
+
+function applyMatrix()
+{
+/*
+	var viewMatrix = [
+		0.5, 0.0, 0.0, 0.0,
+		0.0, 1.0, 0.0, 0.0,
+		0.0, 0.0, 1.0, 0.0,
+		0.0, 0.0, 0.0, 1.0,
+	];
+
+	var projMatrix = [
+		1.0, 0.0, 0.0, 0.0,
+		0.0, 1.0, 0.0, 0.0,
+		0.0, 0.0, 1.0, 0.0,
+		0.0, 0.0, 0.0, 1.0,
+	];
+*/
+	var viewMatrix = mat4.create();
+	var projMatrix = mat4.create();
+
+	var aspect = 640.0/480.0;
+	var near = 0.5;
+	var far = 500.0;
+	mat4.perspective(projMatrix, Math.PI/180.0*45, aspect, near, far);
+
+	var trans = vec3.create();
+	vec3.set(trans, 0,0,-5);
+	mat4.translate(viewMatrix, viewMatrix, trans);
+
+	var uniform = gl.getUniformLocation(shaderProgram, "viewMatrix");
+	gl.uniformMatrix4fv(uniform, false, new Float32Array(viewMatrix));
+
+	uniform = gl.getUniformLocation(shaderProgram, "projMatrix");
+	gl.uniformMatrix4fv(uniform, false, new Float32Array(projMatrix));
+}
+
+
 function drawScene()
 {
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+	applyMatrix();
+	
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+	gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+	gl.vertexAttribPointer(vertexColorAttribute, 4, gl.FLOAT, false, 0, 0);
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 3);
+/*
 	gl.begin(gl.TRIANGLES);
 	gl.vertex(0,0.578,0);
 	gl.vertex(-0.5,-0.288,0);
 	gl.vertex(0.5,-0.288,0);
 	gl.end();
+	*/
 }
 
